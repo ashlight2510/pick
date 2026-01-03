959c0501244c55b7ba110b893a4a4cbaaeb137a5
AshLight <>
2026-01-03 10:43:37 +0900

    chore: add adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1204894220949193" crossOrigin="anonymous"></script>
+      </head>
       <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
